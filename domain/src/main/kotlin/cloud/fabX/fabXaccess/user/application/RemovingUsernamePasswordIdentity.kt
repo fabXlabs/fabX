659b5bb726aa8fefa434323bddd5ed1d75f757ea
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-07-14 11:48:51 +0200

    introduce RemovingUsernamePasswordIdentity service

---
+++ domain/src/main/kotlin/cloud/fabX/fabXaccess/user/application/RemovingUsernamePasswordIdentity.kt
@@ -0,0 +1,44 @@
+package cloud.fabX.fabXaccess.user.application
+
+import arrow.core.Option
+import arrow.core.flatMap
+import cloud.fabX.fabXaccess.DomainModule
+import cloud.fabX.fabXaccess.common.application.logger
+import cloud.fabX.fabXaccess.common.model.Error
+import cloud.fabX.fabXaccess.user.model.Admin
+import cloud.fabX.fabXaccess.user.model.UserId
+import cloud.fabX.fabXaccess.user.model.UsernamePasswordIdentity
+
+/**
+ * Service to remove a [UsernamePasswordIdentity] from a user.
+ */
+class RemovingUsernamePasswordIdentity {
+
+    private val log = logger()
+    private val userRepository = DomainModule.userRepository()
+
+    fun removeUsernamePasswordIdentity(
+        actor: Admin,
+        userId: UserId,
+        username: String
+    ): Option<Error> {
+        log.debug("removeUsernamePasswordIdentity...")
+
+        return userRepository.getById(userId)
+            .flatMap {
+                it.removeUsernamePasswordIdentity(
+                    actor,
+                    username
+                )
+            }
+            .flatMap {
+                userRepository.store(it)
+                    .toEither { }
+                    .swap()
+            }
+            .swap()
+            .orNone()
+            .tapNone { log.debug("...removeUsernamePasswordIdentity done") }
+            .tap { log.error("...removeUsernamePasswordIdentity error: $it") }
+    }
+}
---
+++ domain/src/test/kotlin/cloud/fabX/fabXaccess/user/application/RemovingUsernamePasswordIdentityTest.kt
@@ -0,0 +1,182 @@
+package cloud.fabX.fabXaccess.user.application
+
+import arrow.core.None
+import arrow.core.left
+import arrow.core.right
+import arrow.core.some
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import cloud.fabX.fabXaccess.DomainModule
+import cloud.fabX.fabXaccess.common.model.Error
+import cloud.fabX.fabXaccess.common.model.Logger
+import cloud.fabX.fabXaccess.user.model.AdminFixture
+import cloud.fabX.fabXaccess.user.model.UserFixture
+import cloud.fabX.fabXaccess.user.model.UserIdFixture
+import cloud.fabX.fabXaccess.user.model.UserRepository
+import cloud.fabX.fabXaccess.user.model.UsernamePasswordIdentity
+import cloud.fabX.fabXaccess.user.model.UsernamePasswordIdentityRemoved
+import isNone
+import isSome
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoSettings
+import org.mockito.kotlin.inOrder
+import org.mockito.kotlin.whenever
+
+@MockitoSettings
+internal class RemovingUsernamePasswordIdentityTest {
+
+    private val adminActor = AdminFixture.arbitrary()
+
+    private val userId = UserIdFixture.arbitrary()
+
+    private var logger: Logger? = null
+    private var userRepository: UserRepository? = null
+
+    private var testee: RemovingUsernamePasswordIdentity? = null
+
+    @BeforeEach
+    fun `configure DomainModule`(
+        @Mock logger: Logger,
+        @Mock userRepository: UserRepository
+    ) {
+        this.logger = logger
+        this.userRepository = userRepository
+        DomainModule.configureLoggerFactory { logger }
+        DomainModule.configureUserRepository(userRepository)
+
+        testee = RemovingUsernamePasswordIdentity()
+    }
+
+    @Test
+    fun `given user and identity can be found when removing identity then sourcing event is created and stored`() {
+        // given
+        val username = "username"
+        val password = "password42"
+
+        val user = UserFixture.arbitrary(
+            userId,
+            aggregateVersion = 1,
+            identities = setOf(
+                UsernamePasswordIdentity(username, password)
+            )
+        )
+
+        val expectedSourcingEvent = UsernamePasswordIdentityRemoved(
+            userId,
+            2,
+            adminActor.id,
+            username
+        )
+
+        whenever(userRepository!!.getById(userId))
+            .thenReturn(user.right())
+
+        whenever(userRepository!!.store(expectedSourcingEvent))
+            .thenReturn(None)
+
+        // when
+        val result = testee!!.removeUsernamePasswordIdentity(
+            adminActor,
+            userId,
+            username
+        )
+
+        // then
+        assertThat(result).isNone()
+
+        val inOrder = inOrder(userRepository!!)
+        inOrder.verify(userRepository!!).getById(userId)
+        inOrder.verify(userRepository!!).store(expectedSourcingEvent)
+    }
+
+    @Test
+    fun `given user cannot be found when removing identity then returns error`() {
+        // given
+        val error = Error.UserNotFound("message", userId)
+
+        whenever(userRepository!!.getById(userId))
+            .thenReturn(error.left())
+
+        // when
+        val result = testee!!.removeUsernamePasswordIdentity(
+            adminActor,
+            userId,
+            "username"
+        )
+
+        // then
+        assertThat(result)
+            .isSome()
+            .isEqualTo(error)
+    }
+
+    @Test
+    fun `given sourcing event cannot be stored when adding identity then returns error`() {
+        // given
+        val username = "username"
+
+        val user = UserFixture.arbitrary(
+            userId,
+            aggregateVersion = 1,
+            identities = setOf(
+                UsernamePasswordIdentity(username, "secret42")
+            )
+        )
+
+        val expectedSourcingEvent = UsernamePasswordIdentityRemoved(
+            userId,
+            2,
+            adminActor.id,
+            username
+        )
+
+        val error = Error.UserNotFound("message", userId)
+
+        whenever(userRepository!!.getById(userId))
+            .thenReturn(user.right())
+
+        whenever(userRepository!!.store(expectedSourcingEvent))
+            .thenReturn(error.some())
+
+        // when
+        val result = testee!!.removeUsernamePasswordIdentity(
+            adminActor,
+            userId,
+            username
+        )
+
+        // then
+        assertThat(result)
+            .isSome()
+            .isEqualTo(error)
+    }
+
+    @Test
+    fun `given domain error when removing identity then error is returned`() {
+        // given
+        val user = UserFixture.arbitrary(
+            userId,
+            aggregateVersion = 1,
+            identities = setOf()
+        )
+
+        whenever(userRepository!!.getById(userId))
+            .thenReturn(user.right())
+
+        // when
+        val result = testee!!.removeUsernamePasswordIdentity(
+            adminActor,
+            userId,
+            "unknownUsername"
+        )
+
+        // then
+        assertThat(result)
+            .isSome()
+
+        val inOrder = inOrder(userRepository!!)
+        inOrder.verify(userRepository!!).getById(userId)
+    }
+}
