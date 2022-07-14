6c64276c487d1e922cc71e8597716ed3c0cec17a
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-07-14 11:49:33 +0200

    introduce AddingCardIdentity service

---
+++ domain/src/main/kotlin/cloud/fabX/fabXaccess/user/application/AddingCardIdentity.kt
@@ -0,0 +1,47 @@
+package cloud.fabX.fabXaccess.user.application
+
+import arrow.core.Option
+import arrow.core.flatMap
+import cloud.fabX.fabXaccess.DomainModule
+import cloud.fabX.fabXaccess.common.application.logger
+import cloud.fabX.fabXaccess.common.model.Error
+import cloud.fabX.fabXaccess.user.model.Admin
+import cloud.fabX.fabXaccess.user.model.CardIdentity
+import cloud.fabX.fabXaccess.user.model.UserId
+
+
+/**
+ * Service to add a [CardIdentity] to a user.
+ */
+class AddingCardIdentity {
+
+    private val log = logger()
+    private val userRepository = DomainModule.userRepository()
+
+    fun addCardIdentity(
+        actor: Admin,
+        userId: UserId,
+        cardId: String,
+        cardSecret: String
+    ): Option<Error> {
+        log.debug("addCardIdentity...")
+
+        return userRepository.getById(userId)
+            .map {
+                it.addCardIdentity(
+                    actor,
+                    cardId,
+                    cardSecret
+                )
+            }
+            .flatMap {
+                userRepository.store(it)
+                    .toEither { }
+                    .swap()
+            }
+            .swap()
+            .orNone()
+            .tapNone { log.debug("...addCardIdentity done") }
+            .tap { log.error("...addCardIdentity error: $it") }
+    }
+}
---
+++ domain/src/test/kotlin/cloud/fabX/fabXaccess/user/application/AddingCardIdentityTest.kt
@@ -0,0 +1,148 @@
+package cloud.fabX.fabXaccess.user.application
+
+import arrow.core.None
+import arrow.core.left
+import arrow.core.right
+import arrow.core.some
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import cloud.fabX.fabXaccess.DomainModule
+import cloud.fabX.fabXaccess.common.model.Error
+import cloud.fabX.fabXaccess.common.model.Logger
+import cloud.fabX.fabXaccess.user.model.AdminFixture
+import cloud.fabX.fabXaccess.user.model.CardIdentityAdded
+import cloud.fabX.fabXaccess.user.model.UserFixture
+import cloud.fabX.fabXaccess.user.model.UserIdFixture
+import cloud.fabX.fabXaccess.user.model.UserRepository
+import isNone
+import isSome
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoSettings
+import org.mockito.kotlin.inOrder
+import org.mockito.kotlin.whenever
+
+@MockitoSettings
+internal class AddingCardIdentityTest {
+
+    private val adminActor = AdminFixture.arbitrary()
+
+    private val userId = UserIdFixture.arbitrary()
+
+    private var logger: Logger? = null
+    private var userRepository: UserRepository? = null
+
+    private var testee: AddingCardIdentity? = null
+
+    @BeforeEach
+    fun `configure DomainModule`(
+        @Mock logger: Logger,
+        @Mock userRepository: UserRepository
+    ) {
+        this.logger = logger
+        this.userRepository = userRepository
+        DomainModule.configureLoggerFactory { logger }
+        DomainModule.configureUserRepository(userRepository)
+
+        testee = AddingCardIdentity()
+    }
+
+    @Test
+    fun `given user can be found when adding identity then sourcing event is created and stored`() {
+        // given
+        val user = UserFixture.arbitrary(userId, aggregateVersion = 1)
+
+        val cardId = "11223344556677"
+        val cardSecret = "0123456789abcdef"
+
+        val expectedSourcingEvent = CardIdentityAdded(
+            userId,
+            2,
+            adminActor.id,
+            cardId,
+            cardSecret
+        )
+
+        whenever(userRepository!!.getById(userId))
+            .thenReturn(user.right())
+
+        whenever(userRepository!!.store(expectedSourcingEvent))
+            .thenReturn(None)
+
+        // when
+        val result = testee!!.addCardIdentity(
+            adminActor,
+            userId,
+            cardId,
+            cardSecret
+        )
+
+        // then
+        assertThat(result).isNone()
+
+        val inOrder = inOrder(userRepository!!)
+        inOrder.verify(userRepository!!).getById(userId)
+        inOrder.verify(userRepository!!).store(expectedSourcingEvent)
+    }
+
+    @Test
+    fun `given user cannot be found when adding identity then returns error`() {
+        // given
+        val error = Error.UserNotFound("message", userId)
+
+        whenever(userRepository!!.getById(userId))
+            .thenReturn(error.left())
+
+        // when
+        val result = testee!!.addCardIdentity(
+            adminActor,
+            userId,
+            "77665544332211",
+            "123456789012345678901234567890"
+        )
+
+        // then
+        assertThat(result)
+            .isSome()
+            .isEqualTo(error)
+    }
+
+    @Test
+    fun `given sourcing event cannot be stored when adding identity then returns error`() {
+        // given
+        val user = UserFixture.arbitrary(userId, aggregateVersion = 1)
+
+        val cardId = "77665544332211"
+        val cardSecret = "123456789012345678901234567890abcdef"
+
+        val expectedSourcingEvent = CardIdentityAdded(
+            userId,
+            2,
+            adminActor.id,
+            cardId,
+            cardSecret
+        )
+
+        val error = Error.UserNotFound("message", userId)
+
+        whenever(userRepository!!.getById(userId))
+            .thenReturn(user.right())
+
+        whenever(userRepository!!.store(expectedSourcingEvent))
+            .thenReturn(error.some())
+
+        // when
+        val result = testee!!.addCardIdentity(
+            adminActor,
+            userId,
+            cardId,
+            cardSecret
+        )
+
+        // then
+        assertThat(result)
+            .isSome()
+            .isEqualTo(error)
+    }
+}
