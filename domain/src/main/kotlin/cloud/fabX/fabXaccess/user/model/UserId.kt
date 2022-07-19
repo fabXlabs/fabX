bb3a41af956c468c5dbb8108c6e944b97f729c51
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-07-19 14:12:25 +0200

    add todo

--- domain/src/main/kotlin/cloud/fabX/fabXaccess/user/model/UserId.kt
+++ domain/src/main/kotlin/cloud/fabX/fabXaccess/user/model/UserId.kt
@@ -4,6 +4,8 @@ import cloud.fabX.fabXaccess.common.model.ActorId
 import cloud.fabX.fabXaccess.common.model.EntityId
 import java.util.UUID
 
+// TODO move Ids to common package (disallow importing aggregate2.model.* in aggregate1.model.*)?
+
 /**
  * Technical (artificial) ID of a User.
  */
