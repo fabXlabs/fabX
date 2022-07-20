ec345c81c6f5584510b513a0aa733e42c5bb00b0
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-07-20 22:41:13 +0200

    add ktor-server-test-host dependency

--- rest/build.gradle.kts
+++ rest/build.gradle.kts
@@ -11,4 +11,5 @@ dependencies {
     implementation("io.ktor:ktor-server-core:$ktorVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
 }
