54ce9be145cf79ab6bb4d91955437b7114274d68
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-07-20 06:09:50 +0200

    add AttachingTool service

---
+++ domain/src/main/kotlin/cloud/fabX/fabXaccess/device/application/AttachingTool.kt
@@ -0,0 +1,48 @@
+package cloud.fabX.fabXaccess.device.application
+
+import arrow.core.Option
+import arrow.core.flatMap
+import cloud.fabX.fabXaccess.DomainModule
+import cloud.fabX.fabXaccess.common.application.logger
+import cloud.fabX.fabXaccess.common.model.Error
+import cloud.fabX.fabXaccess.device.model.DeviceId
+import cloud.fabX.fabXaccess.tool.model.ToolId
+import cloud.fabX.fabXaccess.user.model.Admin
+
+/**
+ * Service for attaching a tool to a device.
+ */
+class AttachingTool {
+
+    private val log = logger()
+    private val deviceRepository = DomainModule.deviceRepository()
+    private val toolRepository = DomainModule.toolRepository()
+
+    fun attachTool(
+        actor: Admin,
+        deviceId: DeviceId,
+        pin: Int,
+        toolId: ToolId
+    ): Option<Error> {
+        log.debug("attachTool...")
+
+        return deviceRepository.getById(deviceId)
+            .flatMap {
+                it.attachTool(
+                    actor,
+                    pin,
+                    toolId,
+                    toolRepository
+                )
+            }
+            .flatMap {
+                deviceRepository.store(it)
+                    .toEither { }
+                    .swap()
+            }
+            .swap()
+            .orNone()
+            .tapNone { log.debug("...attachTool done") }
+            .tap { log.error("...attachTool error: $it") }
+    }
+}
---
+++ domain/src/test/kotlin/cloud/fabX/fabXaccess/device/application/AttachingToolTest.kt
@@ -0,0 +1,189 @@
+package cloud.fabX.fabXaccess.device.application
+
+import arrow.core.None
+import arrow.core.left
+import arrow.core.right
+import arrow.core.some
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import cloud.fabX.fabXaccess.DomainModule
+import cloud.fabX.fabXaccess.common.model.Error
+import cloud.fabX.fabXaccess.common.model.ErrorFixture
+import cloud.fabX.fabXaccess.common.model.Logger
+import cloud.fabX.fabXaccess.device.model.DeviceFixture
+import cloud.fabX.fabXaccess.device.model.DeviceIdFixture
+import cloud.fabX.fabXaccess.device.model.DeviceRepository
+import cloud.fabX.fabXaccess.device.model.ToolAttached
+import cloud.fabX.fabXaccess.tool.model.ToolFixture
+import cloud.fabX.fabXaccess.tool.model.ToolIdFixture
+import cloud.fabX.fabXaccess.tool.model.ToolRepository
+import cloud.fabX.fabXaccess.user.model.AdminFixture
+import isNone
+import isSome
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoSettings
+import org.mockito.kotlin.whenever
+
+@MockitoSettings
+internal class AttachingToolTest {
+
+    private val adminActor = AdminFixture.arbitrary()
+
+    private val deviceId = DeviceIdFixture.arbitrary()
+
+    private var logger: Logger? = null
+    private var deviceRepository: DeviceRepository? = null
+    private var toolRepository: ToolRepository? = null
+
+    private var testee: AttachingTool? = null
+
+    @BeforeEach
+    fun `configure DomainModule`(
+        @Mock logger: Logger,
+        @Mock deviceRepository: DeviceRepository,
+        @Mock toolRepository: ToolRepository
+    ) {
+        this.logger = logger
+        this.deviceRepository = deviceRepository
+        this.toolRepository = toolRepository
+        DomainModule.configureLoggerFactory { logger }
+        DomainModule.configureDeviceRepository(deviceRepository)
+        DomainModule.configureToolRepository(toolRepository)
+
+        testee = AttachingTool()
+    }
+
+    @Test
+    fun `given device can be found when attaching tool then sourcing event is created and stored`() {
+        // given
+        val device = DeviceFixture.arbitrary(deviceId, aggregateVersion = 1)
+
+        val toolId = ToolIdFixture.arbitrary()
+        val tool = ToolFixture.arbitrary(toolId)
+
+        val pin = 42
+
+        val expectedSourcingEvent = ToolAttached(
+            deviceId,
+            2,
+            adminActor.id,
+            pin,
+            toolId
+        )
+
+        whenever(deviceRepository!!.getById(deviceId))
+            .thenReturn(device.right())
+
+        whenever(toolRepository!!.getToolById(toolId))
+            .thenReturn(tool.right())
+
+        whenever(deviceRepository!!.store(expectedSourcingEvent))
+            .thenReturn(None)
+
+        // when
+        val result = testee!!.attachTool(
+            adminActor,
+            deviceId,
+            pin,
+            toolId
+        )
+
+        // then
+        assertThat(result).isNone()
+    }
+
+    @Test
+    fun `given domain error when attaching tool then returns domain error`() {
+        // given
+        val device = DeviceFixture.arbitrary(deviceId, aggregateVersion = 1)
+
+        val toolId = ToolIdFixture.arbitrary()
+
+        val expectedDomainError = ErrorFixture.arbitrary()
+
+        whenever(deviceRepository!!.getById(deviceId))
+            .thenReturn(device.right())
+
+        whenever(toolRepository!!.getToolById(toolId))
+            .thenReturn(expectedDomainError.left())
+
+        // when
+        val result = testee!!.attachTool(
+            adminActor,
+            deviceId,
+            1,
+            toolId
+        )
+
+        // then
+        assertThat(result)
+            .isSome()
+            .isEqualTo(expectedDomainError)
+    }
+
+    @Test
+    fun `given device cannot be found when attaching tool then returns error`() {
+        // given
+        val error = Error.DeviceNotFound("", deviceId)
+
+        whenever(deviceRepository!!.getById(deviceId))
+            .thenReturn(error.left())
+
+        // when
+        val result = testee!!.attachTool(
+            adminActor,
+            deviceId,
+            1,
+            ToolIdFixture.arbitrary()
+        )
+
+        // then
+        assertThat(result)
+            .isSome()
+            .isEqualTo(error)
+    }
+
+    @Test
+    fun `given sourcing event cannot be stored when attaching tool then returns error`() {
+        val device = DeviceFixture.arbitrary(deviceId, aggregateVersion = 1)
+
+        val toolId = ToolIdFixture.arbitrary()
+        val tool = ToolFixture.arbitrary(toolId)
+
+        val pin = 42
+
+        val expectedSourcingEvent = ToolAttached(
+            deviceId,
+            2,
+            adminActor.id,
+            pin,
+            toolId
+        )
+
+        val error = ErrorFixture.arbitrary()
+
+        whenever(deviceRepository!!.getById(deviceId))
+            .thenReturn(device.right())
+
+        whenever(toolRepository!!.getToolById(toolId))
+            .thenReturn(tool.right())
+
+        whenever(deviceRepository!!.store(expectedSourcingEvent))
+            .thenReturn(error.some())
+
+        // when
+        val result = testee!!.attachTool(
+            adminActor,
+            deviceId,
+            pin,
+            toolId
+        )
+
+        // then
+        assertThat(result)
+            .isSome()
+            .isEqualTo(error)
+    }
+}
