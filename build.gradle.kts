dcb6a8feb1cbcb8aa1cbff485a5faa70546078a6
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-07-30 17:20:08 +0200

    add compiler option to enable RequiresOptIn annotation

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ allprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         kotlinOptions {
             jvmTarget = "16"
+            freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
         }
     }
 
