c1c5728fc3b9a3778864bd2df9502b26dcef3720
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-07-30 13:53:51 +0200

    add tests for serialization of ChangeableValue

--- domain/src/test/kotlin/cloud/fabX/fabXaccess/common/model/ChangeableValueSerializationTest.kt
+++ domain/src/test/kotlin/cloud/fabX/fabXaccess/common/model/ChangeableValueSerializationTest.kt
@@ -3,39 +3,28 @@ package cloud.fabX.fabXaccess.common.model
 import assertk.assertThat
 import assertk.assertions.isEqualTo
 import assertk.assertions.isNotNull
+import cloud.fabX.fabXaccess.tool.model.ToolType
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.NullSource
+import org.junit.jupiter.params.provider.ValueSource
 
-// TODO write tests
 internal class ChangeableValueSerializationTest {
 
     @Test
-    fun `generic type get test`() {
+    fun `given serialized ChangeToValue of Int when deserializing then deserializes`() {
         // given
-        val l: List<ChangeableValue<Any>> = listOf(
-            ChangeableValue.ChangeToValueString("hello world"),
-            ChangeableValue.ChangeToValueInt(42),
-        )
+        val value: ChangeableValue<Int> = ChangeableValue.ChangeToValueInt(42)
+        val encoded = Json.encodeToString(value)
 
         // when
-        l.forEach {
-            when (it) {
-                is ChangeableValue.ChangeToValue<*> -> {
-                    when (it.value) {
-                        is String -> println("is string")
-                        is Int -> println("is int")
-                        else -> println("is other type")
-                    }
-                }
-                ChangeableValue.LeaveAsIs -> println("is leave as is")
-            }
-        }
-
+        val decoded = Json.decodeFromString<ChangeableValue<Int>>(encoded)
 
         // then
-
+        assertThat(decoded).isEqualTo(value)
     }
 
     @Test
@@ -47,7 +36,6 @@ internal class ChangeableValueSerializationTest {
         val encoded = Json.encodeToString(value)
 
         // then
-        println(encoded)
         assertThat(encoded).isNotNull()
     }
 
@@ -65,7 +53,45 @@ internal class ChangeableValueSerializationTest {
         assertThat(decoded).isEqualTo(value)
     }
 
-    // TODO ChangeableValue of Int
-    // TODO ChangeableValue of Long
-    // TODO ChangeableValue of an Enum
+    @ParameterizedTest
+    @ValueSource(strings = ["a string"])
+    @NullSource
+    fun `given serialized ChangeToValue of optional String when deserializing then deserializes`(s: String?) {
+        // given
+        val value: ChangeableValue<String?> = ChangeableValue.ChangeToValueOptionalString(s)
+        val encoded = Json.encodeToString(value)
+
+        // when
+        val decoded = Json.decodeFromString<ChangeableValue<String?>>(encoded)
+
+        // then
+        assertThat(decoded).isEqualTo(value)
+    }
+
+    @Test
+    fun `given serialized ChangeToValue of Boolean when deserializing then deserializes`() {
+        // given
+        val value: ChangeableValue<Boolean> = ChangeableValue.ChangeToValueBoolean(true)
+        val encoded = Json.encodeToString(value)
+
+        // when
+        val decoded = Json.decodeFromString<ChangeableValue<Boolean>>(encoded)
+
+        // then
+        assertThat(decoded).isEqualTo(value)
+    }
+
+
+    @Test
+    fun `given serialized ChangeToValue of ToolType when deserializing then deserializes`() {
+        // given
+        val value: ChangeableValue<ToolType> = ChangeableValue.ChangeToValueToolType(ToolType.UNLOCK)
+        val encoded = Json.encodeToString(value)
+
+        // when
+        val decoded = Json.decodeFromString<ChangeableValue<ToolType>>(encoded)
+
+        // then
+        assertThat(decoded).isEqualTo(value)
+    }
 }
