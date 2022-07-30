63c9d2e506e89d8d19f46042c2571521da0a1127
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-07-30 21:03:19 +0200

    remove finished to-do

--- persistence/src/main/kotlin/cloud/fabX/fabXaccess/PersistenceModule.kt
+++ persistence/src/main/kotlin/cloud/fabX/fabXaccess/PersistenceModule.kt
@@ -10,8 +10,6 @@ import org.kodein.di.DI
 import org.kodein.di.bindSingleton
 import org.kodein.di.instance
 
-// TODO database operations on Dispatchers.IO thread pool
-
 val persistenceModule = DI.Module("persistence") {
     bindSingleton { DeviceDatabaseRepository(instance()) }
     bindSingleton { QualificationDatabaseRepository(instance()) }
