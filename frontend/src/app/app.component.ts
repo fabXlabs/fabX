3672057af97c27235f88dd9adf2de4c654135efd
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-08-03 03:39:59 +0200

    set component prefix to 'fabx'

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'fabx-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'fabx-dashboard';
+    title = 'fabX';
 }
