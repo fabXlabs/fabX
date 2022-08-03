da3001ecb1551681083b0e7a9de967f82d07c866
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-08-03 03:43:04 +0200

    import angular HttpClient

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,9 +13,11 @@ import { UsersComponent } from './users/users.component';
     ],
     imports: [
         BrowserModule,
+        HttpClientModule,
         AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
