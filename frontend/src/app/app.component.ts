ca6e0218e34afa97e431f8bd9e991c2219ee3232
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-08-04 08:45:35 +0200

    fix dark mode theming

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from "@angular/common";
 
 @Component({
     selector: 'fabx-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'fabX';
+
+    constructor(@Inject(DOCUMENT) private document: Document) {}
+
+    ngOnInit(): void {
+        let themeLink = this.document.getElementById('app-theme') as HTMLLinkElement;
+
+        const darkModeOn = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+        if (darkModeOn) {
+            themeLink.href = 'dark-theme.css';
+        }
+
+        window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", e => {
+            const darkModeOn = e.matches;
+
+            if (darkModeOn) {
+                themeLink.href = 'theme-dark.css';
+            } else {
+                themeLink.href = 'theme-light.css';
+            }
+        });
+    }
 }
