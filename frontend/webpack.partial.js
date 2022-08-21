60c54df753ee987b78d319edef9c187707a1a7be
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-08-22 00:51:56 +0200

    fix version displayed in frontend on local build

--- frontend/webpack.partial.js
+++ frontend/webpack.partial.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 module.exports = {
     plugins: [
         new webpack.EnvironmentPlugin({
-            "FABX_VERSION": "0.0.0-TODO-webpack"
+            "FABX_VERSION": "0.0.0-LOCAL-SNAPSHOT"
         })
     ]
 }
