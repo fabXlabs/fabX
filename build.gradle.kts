ee6f139f4b5bab28bfdfeefd6bdbf6695126e573
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-09-30 18:11:49 +0200

    bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ val mockitoJunitVersion: String by project
 
 plugins {
     base
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
     jacoco
     id("com.github.ben-manes.versions") version "0.42.0"
 }
