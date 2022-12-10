dbd48e9df8a94fc7b05b44a612f8f6d767a07cca
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-12-10 16:46:41 +0100

    frontend: do not intercept HTTP 401 unauthorized if not logged in
    Before this change, the login component would not see the 401 return code and message and thus could not display it.

--- frontend/src/app/services/logout-on-unauthorized-interceptor.service.ts
+++ frontend/src/app/services/logout-on-unauthorized-interceptor.service.ts
@@ -10,6 +10,7 @@ import {
 import { catchError, Observable, throwError } from "rxjs";
 import { Store } from "@ngxs/store";
 import { Auth } from "../state/auth.actions";
+import { FabxState } from "../state/fabx-state";
 
 @Injectable()
 export class LogoutOnUnauthorizedInterceptorService implements HttpInterceptor {
@@ -18,7 +19,7 @@ export class LogoutOnUnauthorizedInterceptorService implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req).pipe(catchError((err: HttpErrorResponse) => {
-            if (err.status == HttpStatusCode.Unauthorized) {
+            if (err.status == HttpStatusCode.Unauthorized && this.store.selectSnapshot(FabxState.isAuthenticated)) {
                 return this.store.dispatch(Auth.Logout);
             } else {
                 return throwError(() => err);
