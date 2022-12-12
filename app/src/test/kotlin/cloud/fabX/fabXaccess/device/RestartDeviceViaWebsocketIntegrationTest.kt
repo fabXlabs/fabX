70e0418b87de36e7461b94e1d258a66e5c11e5a5
Simon Schäffner <simon.schaeffner@googlemail.com>
2022-12-12 18:30:03 +0100

    add integration test for device returning error on restart command

--- app/src/test/kotlin/cloud/fabX/fabXaccess/device/RestartDeviceViaWebsocketIntegrationTest.kt
+++ app/src/test/kotlin/cloud/fabX/fabXaccess/device/RestartDeviceViaWebsocketIntegrationTest.kt
@@ -7,10 +7,12 @@ import assertk.assertions.isInstanceOf
 import cloud.fabX.fabXaccess.common.adminAuth
 import cloud.fabX.fabXaccess.common.c
 import cloud.fabX.fabXaccess.common.isError
+import cloud.fabX.fabXaccess.common.isJson
 import cloud.fabX.fabXaccess.common.rest.Error
 import cloud.fabX.fabXaccess.common.withTestApp
 import cloud.fabX.fabXaccess.device.ws.DeviceResponse
 import cloud.fabX.fabXaccess.device.ws.DeviceRestartResponse
+import cloud.fabX.fabXaccess.device.ws.ErrorResponse
 import cloud.fabX.fabXaccess.device.ws.RestartDevice
 import cloud.fabX.fabXaccess.device.ws.ServerToDeviceCommand
 import io.ktor.client.call.body
@@ -107,5 +109,54 @@ class RestartDeviceViaWebsocketIntegrationTest {
         }
     }
 
-    // TODO test: given device responds with ErrorResponse when restarting device then returns http ...
+    @Test
+    fun `given device responds with error when restarting device returns error`() = withTestApp {
+        // given
+        val mac = "AABB11CC22DD"
+        val secret = "c8760b55353aa7bfc536f2d29499b549"
+        val deviceId = givenDevice(mac = mac, secret = secret)
+
+        // when
+        c().webSocket("/api/v1/device/ws", {
+            basicAuth(mac, secret)
+        }) {
+            (incoming.receive() as Frame.Text).readText() // greeting text
+
+            val httpResultDeferred = async {
+                c().post("/api/v1/device/$deviceId/restart") {
+                    adminAuth()
+                }
+            }
+
+            val commandText = (incoming.receive() as Frame.Text).readText()
+            val command = Json.decodeFromString<ServerToDeviceCommand>(commandText)
+
+            val errorMessage = "an error occurred"
+            val response = ErrorResponse(
+                command.commandId,
+                errorMessage,
+                mapOf(),
+                null
+            )
+            outgoing.send(Frame.Text(Json.encodeToString<DeviceResponse>(response)))
+
+            val httpResponse = httpResultDeferred.await()
+
+            // then
+            assertThat(command).isInstanceOf(RestartDevice::class)
+
+            assertThat(httpResponse.status).isEqualTo(HttpStatusCode.ServiceUnavailable)
+            assertThat(httpResponse.bodyAsText())
+                .isJson<Error>()
+                .isError(
+                    "UnexpectedDeviceResponse",
+                    "Unexpected device response type.",
+                    mapOf(
+                        "deviceId" to deviceId,
+                        "response" to "ErrorResponse(commandId=${command.commandId}, message=an error occurred, " +
+                                "parameters={}, correlationId=null)"
+                    )
+                )
+        }
+    }
 }
