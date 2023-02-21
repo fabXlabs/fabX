6a53806b6053b3c8028b25798d29165f042a7951
Simon Schäffner <simon.schaeffner@googlemail.com>
2023-02-21 21:08:19 +0100

    bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     base
     kotlin("jvm") version "1.8.10"
     jacoco
-    id("com.github.ben-manes.versions") version "0.45.0"
+    id("com.github.ben-manes.versions") version "0.46.0"
 }
 
 tasks.register<GradleBuild>("stage") {
