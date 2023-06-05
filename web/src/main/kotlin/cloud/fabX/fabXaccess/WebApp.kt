059c2cd3cadfe98aed2449c43b4b7a0ef5a7c35b
Simon Schäffner <simon.schaeffner@googlemail.com>
2023-06-05 16:42:30 +0200

    add health endpoint

--- web/src/main/kotlin/cloud/fabX/fabXaccess/WebApp.kt
+++ web/src/main/kotlin/cloud/fabX/fabXaccess/WebApp.kt
@@ -21,6 +21,7 @@ import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
+import io.ktor.server.application.call
 import io.ktor.server.application.install
 import io.ktor.server.auth.Authentication
 import io.ktor.server.auth.authenticate
@@ -44,6 +45,7 @@ import io.ktor.server.request.ApplicationRequest
 import io.ktor.server.request.httpMethod
 import io.ktor.server.request.path
 import io.ktor.server.response.respond
+import io.ktor.server.routing.get
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
 import io.ktor.server.websocket.WebSockets
@@ -211,6 +213,10 @@ class WebApp(
             }
             authenticate("metrics-basic") {
                 metricsController.routes(this)
+            }
+            get("/health") {
+                // TODO expand health checks, e.g. connection to database
+                call.respond(HttpStatusCode.OK)
             }
         }
     }
