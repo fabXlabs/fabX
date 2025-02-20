5d380b87a9a84048d780e2567ce008a02b92ac88
Simon Schäffner <simon.schaeffner@googlemail.com>
2025-02-20 17:24:54 +0100

    fe: add shadcn, login form

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,20 +1,22 @@
 <script lang="ts">
 	import { loginBasicAuth } from '$lib/auth';
+	// noinspection ES6UnusedImports
+	import * as Card from '$lib/components/ui/card';
+	import { Button } from '$lib/components/ui/button';
+	import { Label } from '$lib/components/ui/label';
+	import { Input } from '$lib/components/ui/input';
+	import { goto } from '$app/navigation';
 
 	let username = $state('');
 	let password = $state('');
 
 	let error = $state('');
 
-	let loggedIn = $state(false);
-
 	async function login() {
 		error = '';
 		await loginBasicAuth(username, password)
 			.then(() => {
-				username = '';
-				password = '';
-				loggedIn = true;
+				goto('/qualification');
 			})
 			.catch(e => {
 				error = JSON.stringify(e);
@@ -22,33 +24,34 @@
 	}
 </script>
 
-<div class="prose">
-	<h1>Login</h1>
+<div class="flex h-screen w-full items-center justify-center px-4">
+	<Card.Root class="min-w-72">
+		<Card.Header>
+			<Card.Title class="text-2xl">fabX Login</Card.Title>
+		</Card.Header>
+		<Card.Content>
+			<form>
+				<div class="grid gap-4">
+					<div class="grid gap-2">
+						<Label for="username">Username</Label>
+						<Input bind:value="{username}" type="text" id="username" placeholder="Username" autocomplete="username"
+									 required />
+					</div>
+					<div class="grid gap-2">
+						<Label for="password">Password</Label>
+						<Input bind:value="{password}" type="password" id="password" autocomplete="current-password"
+									 required />
+					</div>
 
-	<form>
-		<label>
-			Username:
-			<input bind:value="{username}" type="text" name="username" autocomplete="username" required />
-		</label>
+					{#if error}
+						<div>
+							<p class="text-sm text-red-600">Error: {error}</p>
+						</div>
+					{/if}
 
-		<div>
-			<label>
-				Password:
-				<input bind:value="{password}" type="password" name="password" autocomplete="current-password" required />
-			</label>
-		</div>
-
-		<button onclick={login} type="submit">Login</button>
-	</form>
-
-	{#if loggedIn}
-		<div>
-			<h2>Logged in!</h2>
-			<a href="/qualification">Qualifications</a>
-		</div>
-	{/if}
-
-	{#if error}
-		<div>Error: {error}</div>
-	{/if}
+					<Button onclick={login} type="submit">Login</Button>
+				</div>
+			</form>
+		</Card.Content>
+	</Card.Root>
 </div>
