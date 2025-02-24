ff5fcd3852b0658ed384ceec1dd5cdc0bf781ead
Simon Schäffner <simon.schaeffner@googlemail.com>
2025-02-24 22:30:54 +0100

    fe: add mobile nav

--- frontend/src/lib/components/site-header.svelte
+++ frontend/src/lib/components/site-header.svelte
@@ -1,22 +1,46 @@
+<script lang="ts">
+	// noinspection ES6UnusedImports
+	import * as Sheet from "$lib/components/ui/sheet/index.js";
+	import NavLink from "./nav-link.svelte";
+	import { CircleUserRound, Menu } from 'lucide-svelte';
+
+	let sheetOpen = $state(false);
+</script>
+
 <header class="border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
 	<div class="container flex h-14 max-w-screen-2xl items-center font-mono text-2xl">
-		<!-- Main Nav -->
 		<div class="mr-4 flex">
+			<!-- Mobile Nav -->
+			<Sheet.Root bind:open={sheetOpen}>
+				<Sheet.Trigger class="mr-2 px-0 text-center text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 sm:hidden">
+					<Menu size="32" class="mt-1 mr-2 hover:text-foreground/80 transition-colors text-foreground/60"/>
+					<span class="sr-only">Toggle Menu</span>
+				</Sheet.Trigger>
+				<Sheet.Content side="left" class="flex flex-col text-2xl">
+					<span class="font-bold text-violet-800 transition-colors font-accent italic text-4xl">fabX</span>
+					<NavLink bind:sheetOpen={sheetOpen} classes="mt-4" href="/admin/user">Users</NavLink>
+					<NavLink bind:sheetOpen={sheetOpen} classes="mt-4" href="/admin/tool">Tools</NavLink>
+					<NavLink bind:sheetOpen={sheetOpen} classes="mt-4" href="/admin/qualification">Qualifications</NavLink>
+					<NavLink bind:sheetOpen={sheetOpen} classes="mt-4" href="/admin/device">Devices</NavLink>
+				</Sheet.Content>
+			</Sheet.Root>
+
+			<!-- Main Nav -->
 			<a href="/admin" class="mr-6 flex items-center space-x-2">
 				<span class="font-bold hover:text-violet-800 transition-colors font-accent italic">fabX</span>
 			</a>
-			<nav class="flex items-center gap-6 text-base">
-				<a class="hover:text-foreground/80 transition-colors text-foreground/60 font-accent" href="/admin/user">Users</a>
-				<a class="hover:text-foreground/80 transition-colors text-foreground/60 font-accent" href="/admin/tool">Tools</a>
-				<a class="hover:text-foreground/80 transition-colors text-foreground/60 font-accent" href="/admin/qualification">Qualifications</a>
-				<a class="hover:text-foreground/80 transition-colors text-foreground/60 font-accent" href="/admin/device">Devices</a>
+			<nav class="hidden sm:flex items-center gap-6 text-base">
+				<NavLink href="/admin/user">Users</NavLink>
+				<NavLink href="/admin/tool">Tools</NavLink>
+				<NavLink href="/admin/qualification">Qualifications</NavLink>
+				<NavLink href="/admin/device">Devices</NavLink>
 			</nav>
 		</div>
-		<!-- TODO Mobile Nav -->
-		<div class="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+		<div class="flex flex-1 items-center space-x-2 justify-end">
 			<nav class="flex items-center gap-6 text-base">
-				<!-- TODO User/Profile Icon -->
-				<a class="hover:text-foreground/80 transition-colors text-foreground/60 font-accent" href="/me">Profile</a>
+				<a class="hover:text-foreground/80 transition-colors text-foreground/60" href="/me">
+					<CircleUserRound class="mt-1" />
+				</a>
 			</nav>
 		</div>
 	</div>
