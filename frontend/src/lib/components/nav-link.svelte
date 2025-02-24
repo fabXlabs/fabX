13beb340a3ed2e8be3de4a50b879698d6e919004
Simon Schäffner <simon.schaeffner@googlemail.com>
2025-02-24 22:30:54 +0100

    fe: add mobile nav

---
+++ frontend/src/lib/components/nav-link.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	let {
+		sheetOpen = $bindable(false),
+		href,
+		children,
+		classes = "",
+		...others
+	} = $props();
+
+	function closeSheet() {
+		sheetOpen = false;
+	}
+</script>
+
+<a class="hover:text-foreground/80 transition-colors text-foreground/60 font-accent {classes}" onclick={closeSheet} {href} {...others}>{@render children()}</a>
