033d6c0ee9fbd0d3a15131d4c933b685e053a03a
Simon Schäffner <simon.schaeffner@googlemail.com>
2025-02-28 17:25:12 +0100

    fe: add routes for devices, tools, users
    also add missing rest models

---
+++ frontend/src/lib/api/model/tool.ts
@@ -0,0 +1,43 @@
+import type { ChangeableValue } from "./changeable-value";
+
+export interface Tool {
+	id: string,
+	aggregateVersion: number,
+	name: string,
+	type: ToolType,
+	requires2FA: boolean,
+	time: number,
+	idleState: IdleState,
+	enabled: Boolean,
+	notes: string | null,
+	wikiLink: string,
+	requiredQualifications: string[]
+}
+
+export interface ToolCreationDetails {
+	name: string,
+	type: ToolType,
+	requires2FA: boolean,
+	time: number,
+	idleState: IdleState,
+	wikiLink: string,
+	requiredQualifications: string[]
+}
+
+export interface ToolDetails {
+	name: ChangeableValue<string> | null,
+	type: ChangeableValue<ToolType> | null,
+	requires2FA: ChangeableValue<boolean> | null,
+	time: ChangeableValue<number> | null,
+	idleState: ChangeableValue<IdleState> | null,
+	enabled: ChangeableValue<boolean> | null,
+	notes: ChangeableValue<string | null> | null,
+	wikiLink: ChangeableValue<string> | null,
+	requiredQualifications: ChangeableValue<string[]> | null,
+}
+
+export const toolTypes = ["UNLOCK", "KEEP"]
+export type ToolType = typeof toolTypes[number];
+
+export const idleStates = ["IDLE_LOW", "IDLE_HIGH"];
+export type IdleState = typeof idleStates[number];
