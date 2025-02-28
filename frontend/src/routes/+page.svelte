5c9a23b870acbb77d71d170cf707131f78bc9280
Simon Schäffner <simon.schaeffner@googlemail.com>
2025-02-28 17:26:47 +0100

    fe: add minimal start page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,4 +1,13 @@
-<article class="prose dark:prose-invert">
-	<h1>Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-</article>
+<script>
+	import { Button } from '$lib/components/ui/button/';
+</script>
+
+<div class="container">
+	<h1 class="font-accent italic text-5xl my-4">fabX</h1>
+	<h2 class="font-accent text-2xl my-2">Access System for Makerspaces</h2>
+
+	<div class="flex flex-col my-4">
+		<Button class="my-4 p-8" href="/admin">Admin</Button>
+		<Button class="my-4 p-8 bg-gray-800 hover:bg-gray-700" href="https://github.com/fabXlabs/fabX" target="_blank" rel="noopener noreferrer">GitHub</Button>
+	</div>
+</div>
