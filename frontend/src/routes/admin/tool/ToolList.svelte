6ddbc57373b771601b717edb2f2d8f9e26b75ac0
Simon Schäffner <simon.schaeffner@googlemail.com>
2025-03-02 16:22:49 +0100

    fe: add ToolList

---
+++ frontend/src/routes/admin/tool/ToolList.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import type { Tool } from '$lib/api/model/tool';
+
+	interface Props {
+		tools: Tool[];
+	}
+
+	let { tools }: Props = $props();
+</script>
+
+
+<table>
+	<thead>
+	<tr>
+		<th>Name</th>
+		<th>Type</th>
+		<th>Enabled</th>
+		<th>2FA</th>
+		<th>Qualifications</th>
+	</tr>
+	</thead>
+	<tbody>
+	{#each tools as tool}
+		<tr>
+			<td>{tool.name}</td>
+			<td>{tool.type}</td>
+			<td>{tool.enabled}</td>
+			<td>{tool.requires2FA}</td>
+			<td>{tool.requiredQualifications}</td>
+		</tr>
+	{/each}
+	</tbody>
+</table>
