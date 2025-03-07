0293051d691d22064c1bf9dd66480e203f85db9e
Simon Schäffner <simon.schaeffner@googlemail.com>
2025-03-07 18:14:59 +0100

    fe: add qualification data table

---
+++ frontend/src/routes/admin/qualification/columns.ts
@@ -0,0 +1,18 @@
+import type { ColumnDef } from '@tanstack/table-core';
+import { renderComponent } from '$lib/components/ui/data-table';
+import type { Qualification } from '$lib/api/model/qualification';
+import QualificationTag from '$lib/components/QualificationTag.svelte';
+
+export const columns: ColumnDef<Qualification>[] = [
+	{
+		accessorKey: 'name',
+		header: 'Name',
+		cell: ({ row }) => {
+			return renderComponent(QualificationTag, { qualification: row.original });
+		}
+	},
+	{
+		accessorKey: 'description',
+		header: 'Description'
+	}
+];
