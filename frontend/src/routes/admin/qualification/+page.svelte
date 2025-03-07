bedd40704d95ca3d5a990185c6077d254d802403
Simon Schäffner <simon.schaeffner@googlemail.com>
2025-03-07 18:14:59 +0100

    fe: add qualification data table

--- frontend/src/routes/admin/qualification/+page.svelte
+++ frontend/src/routes/admin/qualification/+page.svelte
@@ -1,11 +1,29 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
-	import AdminEntityList from '$lib/components/AdminEntityList.svelte';
-	import QualificationList from './QualificationList.svelte';
+	import DataTable from '$lib/components/ui/DataTable.svelte';
+	import { columns } from './columns';
+	import type { SortingState } from '@tanstack/table-core';
+	import { goto } from '$app/navigation';
+	import type { Qualification } from '$lib/api/model/qualification';
 
 	let { data }: PageProps = $props();
+
+	let initialSortingState: SortingState = [
+		{
+			id: 'name',
+			desc: false
+		}
+	];
+
+	function rowClick(data: Qualification) {
+		goto(`/admin/qualification/${data.id}`);
+	}
 </script>
 
-<AdminEntityList name="Qualifications">
-	<QualificationList qualifications={data.qualifications} />
-</AdminEntityList>
+<DataTable
+	{columns}
+	data={data.qualifications}
+	initialColumnVisibility={{}}
+	{initialSortingState}
+	onRowSelect={rowClick}
+/>
