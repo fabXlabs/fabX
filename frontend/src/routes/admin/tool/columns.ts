f814db97b2b5880630a08a09d618787b8699a03c
Simon Schäffner <simon.schaeffner@googlemail.com>
2025-03-07 21:23:30 +0100

    fe: add tool data table

---
+++ frontend/src/routes/admin/tool/columns.ts
@@ -0,0 +1,46 @@
+import type { ColumnDef } from '@tanstack/table-core';
+import type { AugmentedTool } from '$lib/api/model/tool';
+import { renderComponent } from '$lib/components/ui/data-table';
+import QualificationTagList from '$lib/components/QualificationTagList.svelte';
+
+export const columns: ColumnDef<AugmentedTool>[] = [
+	{
+		accessorKey: 'name',
+		header: 'Name'
+	},
+	{
+		accessorKey: 'type',
+		header: 'Type'
+	},
+	{
+		accessorKey: 'requires2FA',
+		header: '2FA'
+	},
+	{
+		accessorKey: 'time',
+		header: 'Time'
+	},
+	{
+		accessorKey: 'idleState',
+		header: 'Idle State'
+	},
+	{
+		accessorKey: 'enabled',
+		header: 'Enabled'
+	},
+	{
+		accessorKey: 'notes',
+		header: 'Notes'
+	},
+	{
+		accessorKey: 'wikiLink',
+		header: 'Wiki'
+	},
+	{
+		accessorKey: 'requiredQualifications',
+		header: 'Qualifications',
+		cell: ({ row }) => {
+			return renderComponent(QualificationTagList, { qualifications: row.original.requiredQualifications || [] })
+		}
+	}
+];
