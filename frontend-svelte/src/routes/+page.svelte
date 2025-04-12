604cfd41b194e0964ec792195ced72cf984e9052
Lennard Schwarz <lennard.schwarz@icloud.com>
2025-04-12 20:27:20 +0200

    Refactor login mechanism to allow multiple roles

--- frontend-svelte/src/routes/+page.svelte
+++ frontend-svelte/src/routes/+page.svelte
@@ -1,16 +1,28 @@
 <script>
 	import { base } from '$app/paths';
-	import { Button } from '$lib/components/ui/button/';
+	import { goto } from '$app/navigation';
+	import bgImg3 from '$lib/assets/bg/3.jpeg';
+	import { LoaderCircle } from 'lucide-svelte';
+	import { onMount } from 'svelte';
 
-  console.log(`base is ${base}`)
+  	console.log(`base is ${base}`)
+
+  	let { data } = $props();
+
+	// route user to their dasboard depending on their role
+	if (data.me) {
+			if (data.me.isAdmin) {
+				goto(`${base}/admin`);
+			} else {
+				goto(`${base}/instructor`);
+			}
+		}
+
+
 </script>
 
-<div class="container">
-	<h1 class="font-accent italic text-5xl my-4">fabX</h1>
-	<h2 class="font-accent text-2xl my-2">Access System for Makerspaces</h2>
-
-	<div class="flex flex-col my-4">
-		<Button class="my-4 p-8" href="{base}/admin">Admin</Button>
-		<Button class="my-4 p-8 bg-gray-800 hover:bg-gray-700" href="https://github.com/fabXlabs/fabX" target="_blank" rel="noopener noreferrer">GitHub</Button>
-	</div>
+<div class="flex grow bg-cover bg-center" style="background-image: url({bgImg3})">
+	  	<div class="flex w-full justify-center items-center">
+			<LoaderCircle class="text-gray-300 w-16 h-16 animate-spin" />
+		</div>
 </div>
