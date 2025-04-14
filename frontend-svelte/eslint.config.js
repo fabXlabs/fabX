99e237f34f9a1fc86b6991c81aaf245d57ad37d9
Lennard Schwarz <lennard.schwarz@icloud.com>
2025-04-14 21:19:30 +0200

    Update eslint config to work with svelte 5

--- frontend-svelte/eslint.config.js
+++ frontend-svelte/eslint.config.js
@@ -30,6 +30,11 @@ export default ts.config(
 				projectService: true,
 				extraFileExtensions: ['.svelte'],
 				parser: ts.parser,
+
+				// We recommend importing and specifying svelte.config.js.
+				// By doing so, some rules in eslint-plugin-svelte will automatically read the configuration and adjust their behavior accordingly.
+				// While certain Svelte settings may be statically loaded from svelte.config.js even if you don’t specify it,
+				// explicitly specifying it ensures better compatibility and functionality.
 				svelteConfig
 			}
 		}
