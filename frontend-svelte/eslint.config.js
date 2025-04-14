2cddc6dddc1e426ae78e3469a04d96281f7c9dc2
Lennard Schwarz <lennard.schwarz@icloud.com>
2025-04-14 21:17:08 +0200

    Refine ESLint config

--- frontend-svelte/eslint.config.js
+++ frontend-svelte/eslint.config.js
@@ -5,6 +5,8 @@ import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
+import svelteConfig from './svelte.config.js';
+
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 export default ts.config(
@@ -12,23 +14,29 @@ export default ts.config(
 	js.configs.recommended,
 	...ts.configs.recommended,
 	...svelte.configs['flat/recommended'],
+	...svelte.configs.prettier,
 	prettier,
 	...svelte.configs['flat/prettier'],
 	{
 		languageOptions: {
-			globals: {
-				...globals.browser,
-				...globals.node
-			}
+			globals: { ...globals.browser, ...globals.node }
 		}
 	},
 	{
-		files: ['**/*.svelte'],
-
+		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
+		// See more details at: https://typescript-eslint.io/packages/parser/
 		languageOptions: {
-			parserOptions: {
-				parser: ts.parser
-			}
+		  parserOptions: {
+			projectService: true,
+			extraFileExtensions: ['.svelte'], 
+			parser: ts.parser,
+	
+			// We recommend importing and specifying svelte.config.js.
+			// By doing so, some rules in eslint-plugin-svelte will automatically read the configuration and adjust their behavior accordingly.
+			// While certain Svelte settings may be statically loaded from svelte.config.js even if you don’t specify it,
+			// explicitly specifying it ensures better compatibility and functionality.
+			svelteConfig
+		  }
 		}
-	}
+	  },
 );
