6cf61afca1b8f87d31a05a20d8e1818701d40c23
Lennard Schwarz <lennard.schwarz@icloud.com>
2025-04-14 21:37:42 +0200

    Remove comments

--- frontend-svelte/eslint.config.js
+++ frontend-svelte/eslint.config.js
@@ -26,17 +26,12 @@ export default ts.config(
 		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
 		// See more details at: https://typescript-eslint.io/packages/parser/
 		languageOptions: {
-		  parserOptions: {
-			projectService: true,
-			extraFileExtensions: ['.svelte'], 
-			parser: ts.parser,
-	
-			// We recommend importing and specifying svelte.config.js.
-			// By doing so, some rules in eslint-plugin-svelte will automatically read the configuration and adjust their behavior accordingly.
-			// While certain Svelte settings may be statically loaded from svelte.config.js even if you don’t specify it,
-			// explicitly specifying it ensures better compatibility and functionality.
-			svelteConfig
-		  }
+			parserOptions: {
+				projectService: true,
+				extraFileExtensions: ['.svelte'],
+				parser: ts.parser,
+				svelteConfig
+			}
 		}
-	  },
+	}
 );
