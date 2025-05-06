24086ddbff3b8189baef7151c73f96f6147fe929
Lennard Schwarz <lennard.schwarz@icloud.com>
2025-05-06 21:25:09 +0200

    [WIP] Copy some stuff

---
+++ frontend-svelte/src/routes/instructor/+page.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	// noinspection ES6UnusedImports
+	import * as Card from '$lib/components/ui/card/index.js';
+	import { base } from '$app/paths';
+	import type { PageProps } from './$types';
+	import { Drill, Microchip, Stamp, Users } from 'lucide-svelte';
+
+	let { data }: PageProps = $props();
+</script>
+
+<svelte:head>
+	<title>fabX | Instructor</title>
+</svelte:head>
+
+<div class="relative container mt-7 max-w-(--breakpoint-2xl)">
+	<h2 class="font-accent my-2 text-2xl">Hello, {data.me?.firstName}</h2>
+
+	<div class="my-6 grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+		<a href="{base}/admin/user">
+			<Card.Root>
+				<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
+					<Card.Title class="font-accent text-sm uppercase">Users</Card.Title>
+					<Users class="text-muted-foreground h-4 w-4" />
+				</Card.Header>
+				<Card.Content>
+					<div class="text-2xl font-bold">{data.userCount}</div>
+				</Card.Content>
+			</Card.Root>
+		</a>
+		<a href="{base}/admin/tool">
+			<Card.Root>
+				<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
+					<Card.Title class="font-accent text-sm uppercase">Tools</Card.Title>
+					<Drill class="text-muted-foreground h-4 w-4" />
+				</Card.Header>
+				<Card.Content>
+					<div class="text-2xl font-bold">{data.toolCount}</div>
+				</Card.Content>
+			</Card.Root>
+		</a>
+		<a href="{base}/admin/qualification">
+			<Card.Root>
+				<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
+					<Card.Title class="font-accent text-sm uppercase">Qualifications</Card.Title>
+					<Stamp class="text-muted-foreground h-4 w-4" />
+				</Card.Header>
+				<Card.Content>
+					<div class="text-2xl font-bold">{data.qualificationCount}</div>
+				</Card.Content>
+			</Card.Root>
+		</a>
+		<a href="{base}/admin/device">
+			<Card.Root>
+				<Card.Header class="flex flex-row items-center justify-between space-y-0 pb-2">
+					<Card.Title class="font-accent text-sm uppercase">Devices</Card.Title>
+					<Microchip class="text-muted-foreground h-4 w-4" />
+				</Card.Header>
+				<Card.Content>
+					<div class="text-2xl font-bold">{data.deviceCount}</div>
+				</Card.Content>
+			</Card.Root>
+		</a>
+	</div>
+</div>
