c818902d9f767c73382f0c0dcb6f892142277276
Simon Schäffner <simon.schaeffner@googlemail.com>
2026-03-16 22:39:43 +0100

    fe: show checkmark on direct unlock success

--- frontend/src/routes/admin/device/[id]/unlock/[toolid]/+page.svelte
+++ frontend/src/routes/admin/device/[id]/unlock/[toolid]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { FabXError } from '$lib/api/model/error';
 	import { unlockTool } from '$lib/api/devices';
-	import { LoaderCircle } from 'lucide-svelte';
+	import { CircleCheckBigIcon, LoaderCircle } from 'lucide-svelte';
 	import type { PageProps } from './$types';
 	import ErrorText from '$lib/components/ErrorText.svelte';
 
@@ -9,21 +9,28 @@
 
 	let error: FabXError | null = $state(null);
 
-	$effect(async () => {
-		await unlockTool_();
+	let unlockSuccess = $state(false);
+
+	$effect(() => {
+		unlockTool_();
 	});
 
 	async function unlockTool_(): Promise<string> {
 		error = null;
-		return await unlockTool(fetch, data.device.id, data.tool.id).catch((e) => {
-			error = e;
-			return '';
-		});
+		return await unlockTool(fetch, data.device.id, data.tool.id)
+			.then((s) => {
+				unlockSuccess = true;
+				return s;
+			})
+			.catch((e) => {
+				error = e;
+				return '';
+			});
 	}
 </script>
 
 <div class="relative container mt-5 max-w-(--breakpoint-2xl)">
-	{#if data.device}
+	{#if data.device && data.tool}
 		<div class="flex justify-between">
 			<div>
 				<h1 class="font-accent mt-4 mb-2 text-3xl">
@@ -35,7 +42,9 @@
 		<div class="my-6 grid gap-4">
 			<div class="my-12 flex grow">
 				<div class="flex w-full items-center justify-center">
-					{#if !error}
+					{#if unlockSuccess}
+						<CircleCheckBigIcon color="green" size="64" />
+					{:else if !error}
 						<LoaderCircle class="h-16 w-16 animate-spin text-gray-300" />
 					{/if}
 
